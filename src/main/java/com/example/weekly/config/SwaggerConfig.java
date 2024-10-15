cb12cff565e1b3f609c934609e9ca50e9151f90a
Dolphin <sonsung2003@gmail.com>
2024-10-15 16:26:31 +0700

    Update swagger ui

--- src/main/java/com/example/weekly/config/SwaggerConfig.java
+++ src/main/java/com/example/weekly/config/SwaggerConfig.java
@@ -1,4 +1,26 @@
 package com.example.weekly.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info().title("API Service Document")
+                .version("1.0.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("weekly")
+                .packagesToScan("com.example.weekly.controller")
+                .build();
+    }
+
 }
--- src/main/java/com/example/weekly/controller/FilmController.java
+++ src/main/java/com/example/weekly/controller/FilmController.java
@@ -3,16 +3,24 @@ package com.example.weekly.controller;
 import com.example.weekly.model.film.dto.FilmDTO;
 import com.example.weekly.model.film.entity.Film;
 import com.example.weekly.repository.FilmRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.Instant;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/film")
+@Validated
+@Slf4j
+@Tag(name = "Film Controller")
 public class FilmController {
     private final FilmRepository filmRepository;
 
@@ -20,14 +28,18 @@ public class FilmController {
         this.filmRepository = filmRepository;
     }
 
+    @Operation(summary = "Get Film Infomation")
     @GetMapping("/{filmId}")
-    public Film getFilm(@PathVariable Short filmId) {
+    public ResponseEntity<Film> getFilm(
+            @PathVariable Short filmId) {
         Optional<Film> film = filmRepository.findById(filmId);
-        return film.orElse(null);
+        return film.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @PostMapping
-    public ResponseEntity<Film> createFilm(@Valid @RequestBody FilmDTO filmDTO) {
+    public ResponseEntity<Film> addFilm(
+            @Valid @RequestBody FilmDTO filmDTO) {
         Film film = Film.builder()
                 .title(filmDTO.getTitle())
                 .description(filmDTO.getDescription())
